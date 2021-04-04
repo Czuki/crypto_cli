ae7ddc0f10c8517ec7c3773eda2fb3c6fbd29f6a
Czuki <bart3322@gmail.com>
2021-04-04 16:11:49 +0200

    requirements added

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     parser, args = create_parser()
 
     if any([args.average_price_by_month, args.consecutive_increase, args.export]):
-        coin_data = CoinPaprikApi(args.start_date, args.end_date, args.coin)
+        coin_data = CoinPaprikApi(args.coin, args.start_date, args.end_date)
         if args.average_price_by_month:
             for month, value in coin_data.get_average().items():
                 print(f'{month} | {value}')
